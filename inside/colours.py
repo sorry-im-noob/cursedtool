9cc03cac46f2b6b11fc796e615211369b248cc6f
sorry-im-noob <81569119+sorry-im-noob@users.noreply.github.com>
2021-03-30 12:29:45 +0800

    Add files via upload

--- inside/colours.py
+++ inside/colours.py
@@ -1,1 +1,10 @@
-
+def colours (curses):
+    if curses.can_change_color ():
+        curses.start_color ()
+        curses.use_default_colors ()
+        curses.init_color (10,96, 978, 121) # lightgreen
+        curses.init_pair (1,10,-1) # main color
+        curses.init_color (11,211, 720, 930) # blue
+        curses.init_pair (2,11,-1) # warning message
+    else:
+        raise Exception ("terminalmu tidak bisa menggunakan warna")
